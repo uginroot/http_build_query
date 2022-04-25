15b4ab2c15aec439d49cf390c1f532f9ae807d86
SorokaAnton <uginroot@gmail.com>
2022-04-25 18:15:51 +0600

    Refactor variable names

--- helper.go
+++ helper.go
@@ -15,23 +15,23 @@ func Encode(data map[string]interface{}) string {
 func encode(data interface{}) string {
 	result := encodeValue(data, "")
 	var params []string
-	for k, v := range result {
-		k = url.QueryEscape(k)
-		switch vv := v.(type) {
+	for key, value := range result {
+		key = url.QueryEscape(key)
+		switch vv := value.(type) {
 		case string:
-			params = append(params, k+"="+url.QueryEscape(vv))
+			params = append(params, key+"="+url.QueryEscape(vv))
 		case uint, int, uint8, int8, uint16, int16, uint32, int32, uint64, int64:
-			params = append(params, k+"="+url.QueryEscape(fmt.Sprintf("%d", vv)))
+			params = append(params, key+"="+url.QueryEscape(fmt.Sprintf("%d", vv)))
 		case float32, float64:
-			params = append(params, k+"="+url.QueryEscape(fmt.Sprintf("%f", vv)))
+			params = append(params, key+"="+url.QueryEscape(fmt.Sprintf("%f", vv)))
 		case bool:
 			if vv {
-				params = append(params, k+"=1")
+				params = append(params, key+"=1")
 			} else {
-				params = append(params, k+"=0")
+				params = append(params, key+"=0")
 			}
 		default:
-			panic(fmt.Sprintf("Failed convert %T to string", v))
+			panic(fmt.Sprintf("Failed convert %T to string", value))
 		}
 	}
 
@@ -41,46 +41,48 @@ func encode(data interface{}) string {
 }
 
 func encodeValue(value interface{}, key string) map[string]interface{} {
-	switch v := value.(type) {
+	switch typedValue := value.(type) {
 	case string, uint, int, uint8, int8, uint16, int16, uint32, int32, uint64, int64, float32, float64, bool:
-		return map[string]interface{}{key: v}
+		return map[string]interface{}{key: typedValue}
 	default:
-		rv := reflect.ValueOf(v)
-		switch rv.Kind() {
+		reflectValue := reflect.ValueOf(typedValue)
+		switch reflectValue.Kind() {
 		case reflect.Slice:
 			result := map[string]interface{}{}
-			for i := 0; i < rv.Len(); i++ {
-				for vkk, vvv := range encodeValue(rv.Index(i).Interface(), fmt.Sprintf("%s[%d]", key, i)) {
-					result[vkk] = vvv
+			for i := 0; i < reflectValue.Len(); i++ {
+				oneDimensionalSlice := encodeValue(reflectValue.Index(i).Interface(), fmt.Sprintf("%s[%d]", key, i))
+				for resultKey, resultValue := range oneDimensionalSlice {
+					result[resultKey] = resultValue
 				}
 			}
 			return result
 		case reflect.Map:
 			result := map[string]interface{}{}
-			for _, ri := range rv.MapKeys() {
-				var kkk string
+			for _, reflectMapKey := range reflectValue.MapKeys() {
+				var prefixKey string
 				if key == "" {
-					kkk = fmt.Sprintf("%v", ri.Interface())
+					prefixKey = fmt.Sprintf("%v", reflectMapKey.Interface())
 				} else {
-					kkk = fmt.Sprintf("%s[%v]", key, ri.Interface())
+					prefixKey = fmt.Sprintf("%s[%v]", key, reflectMapKey.Interface())
 				}
-
-				for vkk, vvv := range encodeValue(rv.MapIndex(ri).Interface(), kkk) {
-					result[vkk] = vvv
+				oneDimensionalMap := encodeValue(reflectValue.MapIndex(reflectMapKey).Interface(), prefixKey)
+				for resultKey, resultValue := range oneDimensionalMap {
+					result[resultKey] = resultValue
 				}
 			}
 			return result
 		case reflect.Struct:
 			result := map[string]interface{}{}
-			for i := 0; i < rv.NumField(); i++ {
-				kkk := fmt.Sprintf("%s[%v]", key, rv.Type().Field(i).Name)
-				for vkk, vvv := range encodeValue(rv.Field(i).Interface(), kkk) {
-					result[vkk] = vvv
+			for i := 0; i < reflectValue.NumField(); i++ {
+				keyPrefix := fmt.Sprintf("%s[%v]", key, reflectValue.Type().Field(i).Name)
+				oneDimensionalStruct := encodeValue(reflectValue.Field(i).Interface(), keyPrefix)
+				for resultKey, resultValue := range oneDimensionalStruct {
+					result[resultKey] = resultValue
 				}
 			}
 			return result
 		default:
-			panic(fmt.Sprintf("Failed convert %T to string", v))
+			panic(fmt.Sprintf("Failed convert %T to string", typedValue))
 		}
 	}
 }
